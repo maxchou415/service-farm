d90e062fc9ccb2e3ae1170eef89cadc40fd1d85a
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2020-07-27 01:30:55 +0800

    - Add errorMessage for failed fetching endpoints

--- infra/utils/responseHandlers/errorMessageMap.js
+++ infra/utils/responseHandlers/errorMessageMap.js
@@ -1,5 +1,7 @@
 module.exports = {
   AUTH_FAILED: { status: 401, message: 'AUTH_FAILED' },
 
+  FETCH_ENDPOINTS_FAILURE: { status: 400, message: 'FETCH_ENDPOINTS_FAILURE' },
+
   BAD_REQUEST: { status: 400, message: 'BAD_REQUEST' }
 }
